d26b2f925ce308bea3bc085ac6356a472e7b6916
faloger <gtllop098@gmail.com>
2025-11-29 11:01:44 +0300

    Task 3 complete

--- collision/rect.py
+++ collision/rect.py
@@ -6,3 +6,12 @@ def isCorrectRect(rect):
     (x1, y1), (x2, y2) = rect  # Распаковка координат точек прямоугольника
     return x1 < x2 and y1 < y2  # Проверка, что первая точка левее и ниже второй
 
+def isCollisionRect(rect1, rect2):
+    if not isCorrectRect(rect1):  # Проверка корректности первого прямоугольника
+        raise RectCorrectError("1й прямоугольник некоректный")
+    if not isCorrectRect(rect2):  # Проверка корректности второго прямоугольника
+        raise RectCorrectError("2й прямоугольник некоректный")
+    (x1, y1), (x2, y2) = rect1  # Распаковка координат первого прямоугольника
+    (a1, b1), (a2, b2) = rect2  # Распаковка координат второго прямоугольника
+    return not (x2 < a1 or a2 < x1 or y2 < b1 or b2 < y1)  # Проверка пересечения прямоугольников
+
